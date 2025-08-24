91ad61975be1ee077ae1735a64fb37515b7f5902
dev <rust.lang.developer@gmail.com>
2025-08-24 21:31:12 +0100

    frontend - expanded CSP to support eval in element/inline contexts and workers

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,10 +3,13 @@ import type { NextConfig } from 'next'
 const csp = [
   "default-src 'self'",
   "script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'",
+  "script-src-elem 'self' 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'",
+  "script-src-attr 'self' 'unsafe-inline'",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src * data: blob:",
   "connect-src 'self' https: wss:",
   "font-src 'self' https: data:",
+  "worker-src 'self' blob:",
   "frame-ancestors 'self'",
 ].join('; ')
 
